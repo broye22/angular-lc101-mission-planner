d955bf62b04822d22ff1b04876e3c432ddb56bb8
B.Roye <rsfbroye@gmail.com>
2021-04-01 21:09:04 -0500

    finally

--- mission-planning-dashboard/src/app/app.module.ts
+++ mission-planning-dashboard/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { EquipmentComponent } from './equipment/equipment.component';
+import { ExperimentsComponent } from './experiments/experiments.component';
+import { CrewComponent } from './crew/crew.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EquipmentComponent,
+    ExperimentsComponent,
+    CrewComponent
   ],
   imports: [
     BrowserModule
